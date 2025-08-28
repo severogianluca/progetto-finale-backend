359c4498b772e883d53e5550938053b849c3dc0a
severogianluca <gianlucasevero300@gmail.com>
2025-08-28 21:22:36 +0200

    Feat: add bootstrap  dependency

---
+++ src/main/java/org/lessons/java/progetto_finale_backend/controller/VideogameController.java
@@ -0,0 +1,10 @@
+package org.lessons.java.progetto_finale_backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/videogames")
+public class VideogameController {
+    
+}
