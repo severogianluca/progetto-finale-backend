33d2dc72ec05dd9e54a4c690790846731e9ceb3b
severogianluca <gianlucasevero300@gmail.com>
2025-09-01 16:18:19 +0200

    Feat: add show

--- src/main/java/org/lessons/java/progetto_finale_backend/controller/VideogameController.java
+++ src/main/java/org/lessons/java/progetto_finale_backend/controller/VideogameController.java
@@ -1,6 +1,7 @@
 package org.lessons.java.progetto_finale_backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.lessons.java.progetto_finale_backend.model.Videogame;
 import org.lessons.java.progetto_finale_backend.service.VideogameService;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,6 +26,20 @@ public class VideogameController {
         model.addAttribute("videogames", videogames);
 
         return "videogames/index";
+    }
+
+        @GetMapping("/{id}")
+    public String getPizzaById(@PathVariable("id") int id, Model model) {
+
+        Optional<Videogame> game = videogameService.findById(id);
+        if (game.isPresent()) {
+            model.addAttribute("game", game.get());
+            // model.addAttribute("ingredienti", ingredienteRepository.findAll());
+
+            return "videogames/show";
+        }
+
+        return "redirect:videogames";
     }
 
 }
